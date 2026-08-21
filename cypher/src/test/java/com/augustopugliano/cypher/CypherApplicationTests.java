ba58d23cd93b8ed35daf459a255a0511a353aa14
Augusto Pugliano <apug2004@gmail.com>
2026-08-21 18:49:20 -0300

    Esqueleto inicial conectado a postgres

---
+++ cypher/src/test/java/com/augustopugliano/cypher/CypherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustopugliano.cypher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
